90a74d6035e3229e6d02cc7a043aa3aaf13eda0d
saka947 <947508470@qq.com>
2018-07-31 20:25:25 +0800

    a django user authentic demo

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=50, blank=True)
+
+    class Meta(AbstractUser.Meta):
+        pass
+
+
+class Profile(models.Model):
+    nickname = models.CharField(max_length=50, blank=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
